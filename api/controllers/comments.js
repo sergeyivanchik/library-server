4fb19ffbc90c5f1d3255d4bfaa21d69d93e84a19
sergeyivanchik <ivanchik-s@mail.ru>
2020-06-17 16:37:23 +0300

    add socket to add comment

--- api/controllers/comments.js
+++ api/controllers/comments.js
@@ -15,19 +15,22 @@ const getCommentsByBook = async (req, res) => {
     });
 };
 
-const addComment = async (req, res) => {
-  const newComment = await new Comment(req.body);
-  newComment.save()
-    .then(comment => res.send(comment))
-    .catch(error => {
-      res.status(500).send({
-        message: error.message
-      });
-      res.send(error);
-    });
+const addComment = async (comment) => {
+  const newComment = await new Comment(comment);
+  return newComment.save()
+    .then(data => getCommentById(data.id))
+    .catch(error => console.log(error));
+};
+
+const getCommentById = async (commentId) => {
+  return await Comment.findById(commentId)
+    .populate('user', 'username')
+    .then(data => data)
+    .catch(error => console.log(error));
 };
 
 module.exports = {
   getCommentsByBook,
-  addComment
+  addComment,
+  getCommentById
 }
